d63164e0bf1dab7a11099552bfb58a0a76002b5c
orsan <orsan.awawdi@gmail.com>
2024-10-06 12:04:12 +0300

    added getenv

--- git_branches/git_branch.py
+++ git_branches/git_branch.py
@@ -2,4 +2,4 @@ import os
 import subprocess
 
 print(subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"], encoding="UTF-8").strip())
-#print(os.getenv("GIT_BRANCH"))
+print(os.getenv("GIT_BRANCH"))
