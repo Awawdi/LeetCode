f5a5ab9ae755f83ad2c1e649f3d2303ee1d0ed4c
orsan <orsan.awawdi@gmail.com>
2024-10-23 12:23:52 +0300

    added shared-library function

--- git_branches/vars/helloWorld.groovy
+++ git_branches/vars/helloWorld.groovy
@@ -1,3 +1,14 @@
-    def call() {
-        sh "echo hello world"
+def call(String name, String email_address) {
+    if (isValidEmail(email_address)) {
+        sh "echo hi ${name}, email is ${email_address}"
     }
+    else {
+        sh "echo Invalid email format: ${email_address}"
+    }
+}
+
+def isValidEmail(String email) {
+
+    def emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    return email ==~ emailPattern
+}
